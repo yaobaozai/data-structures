3ee427345ec2faec21b154730cfe88ee68fcc0fd
yaobaozai <282926102@qq.com>
2018-11-21 22:57:41 +0800

    动态数组

--- Array/src/Array.java
+++ Array/src/Array.java
@@ -32,10 +32,10 @@ public class Array<E> {
   // 在第index个位置插入一个新元素e
   public void add(int index, E e) {
 
-    if(size == data.length)
-      throw new IllegalArgumentException("Add failed. Array is full");
     if(index < 0 || index > size)
       throw new IllegalArgumentException("Add failed. Require index >= 0 && index <= size");
+    if(size == data.length)
+      resize(2 * data.length);
 
     for(int i = size - 1; i >= index; i--)
       data[i + 1] = data[i];
@@ -95,6 +95,9 @@ public class Array<E> {
       data[i - 1] = data[i];
     size --;
     data[size] = null; // loitering objects != momery leak
+
+    if(size == data.length / 2)
+      resize(data.length / 2);
     return ret;
   }
 
@@ -128,6 +131,13 @@ public class Array<E> {
     }
     res.append("]");
     return res.toString();
+  }
+
+  private void resize(int newCapacity) {
+    E[] newData = (E[])new Object[newCapacity];
+    for (int i = 0; i<size;i++)
+      newData[i] = data[i];
+    data = newData;
   }
 
 }
